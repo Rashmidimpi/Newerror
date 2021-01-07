2a88d3f8600d6bc1887373248ddb3c10443d3a0c
rashmi <itsrashmi19@gmail.com>
2021-01-07 16:00:52 +0530

    fisrt

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->integer('mobile_number');
+            $table->string('gender');
+            $table->string('state');
+            $table->string('city');
+            $table->string('district');
+            $table->string('school');
+            $table->string('class');
+            $table->string('school_board');
+            $table->string('bio');
+            $table->string('address');
+            $table->string('facebook_link');
+            $table->string('instagram_link');
+            $table->string('interest');
+            $table->text('image')->nullable();
+            $table->string('role');
+            $table->string('has_child')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
